3e17ecc4ae11d43b7b021d5a99ad480395f79c3c
parker02311 <parker02311@gmail.com>
2023-05-02 19:17:59 -1000

    Add give/revoke product and update docs

--- bot/cogs/api_v1.py
+++ bot/cogs/api_v1.py
@@ -4,8 +4,8 @@
 """
 from discord.ext.commands import Cog
 from discord import app_commands, Interaction
-from fastapi import FastAPI, HTTPException, Depends, WebSocket
-from fastapi.security import OAuth2PasswordBearer
+from fastapi import FastAPI, HTTPException, Depends, WebSocket, Security
+from fastapi.security.api_key import APIKeyHeader, APIKey
 from starlette.responses import RedirectResponse
 from bot import config
 from bot.data import (
@@ -22,9 +22,10 @@ from bot.data import (
     get_tag,
     create_tag,
     delete_tag,
+    is_connected,
 )
 from pydantic import BaseModel
-from typing import Union
+from typing import Union, Optional
 from datetime import datetime
 from .. import __version__ as version
 import uvicorn
@@ -33,16 +34,54 @@ import random
 import string
 
 _log = logging.getLogger(__name__)
-app = FastAPI()
+description = """
+Redon Hub is a product delivery system (A.K.A. hub) for Roblox. This bot is [open source](https://github.com/Redon-Tech/Redon-Hub)!
+
+* Users (Partially Implemented, See Below)
+* Products (Fully Implemented)
+* Tags (Fully Implemented)
+
+### Users (Partially Implemented)
+The reason why this is currently partially implemented is because give/revoke product enpoints do not count as purchases.
+This means the product will only get its owners increased/decreased not purchases, as of now only purchases can be increased using the websocket.
+In a future update I plan on resolving this.
+
+[Official Documentation](https://hub.redon.tech/docs)
+"""
+app = FastAPI(
+    title="Redon Hub",
+    description=description,
+    version=version,
+    contact={
+        "name": "Parker",
+        "url": "https://parker02311.redon.tech/contact-me.html",
+        "email": "parker02311@redon.tech",
+    },
+    license_info={"name": "MIT", "url": "https://mit-license.org/"},
+    openapi_tags=[
+        {
+            "name": "Users",
+            "description": "Users are the people who use the hub. Users are based around Roblox id and have an optional link to a Discord id.",
+        },
+        {
+            "name": "Products",
+            "description": "Products are the items that are sold on the hub. Products are based around id's however most endpoints can retrieve them by name as names have to be unique.",
+        },
+        {
+            "name": "Tags",
+            "description": "Tags are used to categorize products. Tags are based around id's however most endpoints can retrieve them by name as names have to be unique.",
+        },
+    ],
+)
 app.logger = _log
-cog = None
-X_API_KEY = OAuth2PasswordBearer(config.API.Key)
+# cog = None
+X_API_KEY = APIKeyHeader(name="Authorization", auto_error=False)
 verificationKeys = {}
 
 
-def api_auth(x_api_key: str = Depends(X_API_KEY)):
-    if x_api_key != config.API.Key:
-        raise HTTPException(status_code=401, detail="Invalid API Key.")
+def api_auth(x_api_key: str = Security(X_API_KEY)):
+    if x_api_key != f"Bearer {config.API.Key}":
+        raise HTTPException(status_code=403, detail="Invalid API Key.")
 
     return x_api_key
 
@@ -76,8 +115,10 @@ class ProductDisplay(BaseModel):
     createdAt: datetime
     name: str
     description: str
+    imageId: str
     price: int
     productId: int
+    stock: Optional[int]
     attachments: list[str]
     tags: list[int]
     purchases: int
@@ -87,15 +128,24 @@ class ProductDisplay(BaseModel):
 class Product(BaseModel):
     name: Union[str, None] = None
     description: Union[str, None] = None
+    imageId: Union[str, None] = None
     price: Union[float, None] = None
     productId: Union[float, None] = None
+    stock: Union[int, None] = None
     attachments: Union[int, None] = None
     tags: Union[int, None] = None
 
 
+class Verification(BaseModel):
+    message: str
+    data: Optional[str] = "key123"
+
+
 @app.get("/")
 async def root():
-    return {"message": "Online", "Version": cog.bot.version}
+    """
+    Returns the status of the API and database as well as the version the bot is running.
+    """
     return {
         "message": "Online",
         "databaseConnected": is_connected(),
@@ -131,11 +181,19 @@ async def websocket_endpoint(websocket: WebSocket):
 ## Users
 @app.get("/v1")
 async def v1root():
+    """
+    Redirections to /
+    """
     return RedirectResponse(url="/")
 
 
-@app.get("/v1/users", dependencies=[Depends(api_auth)])
+@app.get("/v1/users", dependencies=[Depends(api_auth)], tags=["Users"])
 async def users_get() -> dict[int, UserDisplay]:
+    """
+    Gets all users in the database.
+
+    If you have a specific user to get data on please use /v1/users/{user_id} it is much faster.
+    """
     try:
         users = await get_users()
     except Exception as e:
@@ -149,8 +207,11 @@ async def users_get() -> dict[int, UserDisplay]:
     return results
 
 
-@app.get("/v1/users/{user_id}", dependencies=[Depends(api_auth)])
+@app.get("/v1/users/{user_id}", dependencies=[Depends(api_auth)], tags=["Users"])
 async def users_get_user(user_id: int, discordId: bool = False) -> UserDisplay:
+    """
+    Gets a specific user by their id or discordId.
+    """
     try:
         if discordId:
             user = await get_user_by_discord_id(user_id)
@@ -162,8 +223,15 @@ async def users_get_user(user_id: int, discordId: bool = False) -> UserDisplay:
     return UserDisplay(**user.dict())
 
 
-@app.get("/v1/users/{user_id}/owns/{product_id}")
-async def users_get_user_owns(user_id: int, product_id: Union[int, str], discordId: bool = False) -> bool:
+@app.get("/v1/users/{user_id}/owns/{product_id}", tags=["Users"])
+async def users_get_user_owns(
+    user_id: int, product_id: Union[int, str], discordId: bool = False
+) -> bool:
+    """
+    Returns if a user owns a product, very useful for whitelist systems.
+
+    **Does not require a API Key**
+    """
     try:
         if discordId:
             user = await get_user_by_discord_id(user_id)
@@ -178,14 +246,98 @@ async def users_get_user_owns(user_id: int, product_id: Union[int, str], discord
             product_id = product_info.id
         except Exception as e:
             return False
-    
+
     if product_id in user.purchases:
         return True
 
     return False
 
-@app.post("/v1/users/{user_id}/verify", dependencies=[Depends(api_auth)])
-async def users_post_verify(user_id: int) -> dict:
+
+@app.post(
+    "/v1/users/{user_id}/{product_id}", dependencies=[Depends(api_auth)], tags=["Users"]
+)
+async def users_give_user_product(
+    user_id: int, product_id: Union[int, str], discordId: bool = False
+) -> UserDisplay:
+    """
+    Gives a user a specific product, please note this does not increment the purchases counter on the product.
+    """
+    try:
+        if discordId:
+            user = await get_user_by_discord_id(user_id)
+        else:
+            user = await get_user(user_id)
+    except Exception as e:
+        return False
+
+    if type(product_id) == str:
+        try:
+            product_info = await get_product_by_name(product_id)
+            product_id = product_info.id
+        except Exception as e:
+            raise HTTPException(status_code=404, detail="Product Not Found")
+
+    try:
+        product = await get_product(product_id)
+
+        if product.id not in user.purchases:
+            user.purchases.append(product.id)
+            await user.push()
+            product.owners += 1
+            await product.push()
+
+        return UserDisplay(**user.dict())
+    except Exception as e:
+        _log.error(e)
+        raise HTTPException(status_code=500, detail="Internal Server Error")
+
+
+@app.delete(
+    "/v1/users/{user_id}/{product_id}", dependencies=[Depends(api_auth)], tags=["Users"]
+)
+async def users_revoke_user_product(
+    user_id: int, product_id: Union[int, str], discordId: bool = False
+) -> UserDisplay:
+    """
+    Revokes a users product.
+    """
+    try:
+        if discordId:
+            user = await get_user_by_discord_id(user_id)
+        else:
+            user = await get_user(user_id)
+    except Exception as e:
+        return False
+
+    if type(product_id) == str:
+        try:
+            product_info = await get_product_by_name(product_id)
+            product_id = product_info.id
+        except Exception as e:
+            raise HTTPException(status_code=404, detail="Product Not Found")
+
+    try:
+        product = await get_product(product_id)
+
+        if product.id in user.purchases:
+            user.purchases.remove(product.id)
+            await user.push()
+            product.owners -= 1
+            await product.push()
+
+        return UserDisplay(**user.dict())
+    except Exception as e:
+        _log.error(e)
+        raise HTTPException(status_code=500, detail="Internal Server Error")
+
+
+@app.post(
+    "/v1/users/{user_id}/verify", dependencies=[Depends(api_auth)], tags=["Users"]
+)
+async def users_post_verify(user_id: int) -> Verification:
+    """
+    Get a verification key for a user.
+    """
     try:
         user = await get_user(user_id)
     except Exception as e:
@@ -194,14 +346,21 @@ async def users_post_verify(user_id: int) -> dict:
     if not user or user.discordId == 0:
         key = "".join(random.choices(string.ascii_letters + string.digits, k=5))
         verificationKeys[key] = user_id
-        return {"message": "Verification Key Created", "data": key}
+        # return {"message": "Verification Key Created", "data": key}
+        return Verification(message="Verification Key Created", data=key)
     else:
-        return {"message": "User Already Verified"}
+        # return {"message": "User Already Verified"}
+        return Verification(message="User Already Verified")
 
 
 ## Products
-@app.get("/v1/products", dependencies=[Depends(api_auth)])
+@app.get("/v1/products", dependencies=[Depends(api_auth)], tags=["Products"])
 async def products_get() -> dict[int, ProductDisplay]:
+    """
+    Gets all products in the database.
+
+    If you have a specific product to get data on please use /v1/products/{user_id} it is much faster.
+    """
     try:
         products = await get_products()
     except Exception as e:
@@ -215,8 +374,13 @@ async def products_get() -> dict[int, ProductDisplay]:
     return results
 
 
-@app.get("/v1/products/{product_id}", dependencies=[Depends(api_auth)])
+@app.get(
+    "/v1/products/{product_id}", dependencies=[Depends(api_auth)], tags=["Products"]
+)
 async def products_get_product(product_id: Union[int, str]) -> ProductDisplay:
+    """
+    Gets a specific product from the database.
+    """
     if type(product_id) == str:
         try:
             product_info = await get_product_by_name(product_id)
@@ -232,14 +396,19 @@ async def products_get_product(product_id: Union[int, str]) -> ProductDisplay:
     return ProductDisplay(**product.dict())
 
 
-@app.post("/v1/products", dependencies=[Depends(api_auth)])
+@app.post("/v1/products", dependencies=[Depends(api_auth)], tags=["Products"])
 async def products_post(product: Product) -> ProductDisplay:
+    """
+    Creates a new product.
+    """
     try:
         product = await create_product(
             product.name,
             product.description,
+            product.imageId,
             product.price,
             product.productId,
+            product.stock,
             product.attachments,
             product.tags,
         )
@@ -250,10 +419,17 @@ async def products_post(product: Product) -> ProductDisplay:
     return ProductDisplay(**product.dict())
 
 
-@app.patch("/v1/products/{product_id}", dependencies=[Depends(api_auth)])
+@app.patch(
+    "/v1/products/{product_id}", dependencies=[Depends(api_auth)], tags=["Products"]
+)
 async def products_patch(
     product_id: Union[int, str], product: Product
 ) -> ProductDisplay:
+    """
+    Updates a product.
+
+    This is a patch meaning you don't have to pass all the data in the product object.
+    """
     if type(product_id) == str:
         try:
             product_info = await get_product_by_name(product_id)
@@ -277,8 +453,15 @@ async def products_patch(
     return ProductDisplay(**product.dict())
 
 
-@app.delete("/v1/products/{product_id}", dependencies=[Depends(api_auth)])
-async def products_delete(product_id: Union[int, str]) -> dict:
+@app.delete(
+    "/v1/products/{product_id}", dependencies=[Depends(api_auth)], tags=["Products"]
+)
+async def products_delete(product_id: Union[int, str]) -> bool:
+    """
+    Deletes a product.
+
+    **CAN NOT BE UNDONE!**
+    """
     if type(product_id) == str:
         try:
             product_info = await get_product_by_name(product_id)
@@ -292,12 +475,17 @@ async def products_delete(product_id: Union[int, str]) -> dict:
         _log.error(e)
         raise HTTPException(status_code=500, detail="Internal Server Error")
 
-    return {"message": "Product Deleted"}
+    return True
 
 
 ## Tags
-@app.get("/v1/tags", dependencies=[Depends(api_auth)])
+@app.get("/v1/tags", dependencies=[Depends(api_auth)], tags=["Tags"])
 async def tags_get() -> dict[int, TagDisplay]:
+    """
+    Gets all tags in the database.
+
+    If you have a specific tag to get data on please use /v1/tags/{tag_id} it is much faster.
+    """
     try:
         tags = await get_tags()
     except Exception as e:
@@ -311,8 +499,11 @@ async def tags_get() -> dict[int, TagDisplay]:
     return results
 
 
-@app.get("/v1/tags/{tag_id}", dependencies=[Depends(api_auth)])
+@app.get("/v1/tags/{tag_id}", dependencies=[Depends(api_auth)], tags=["Tags"])
 async def tags_get_tag(tag_id: int) -> TagDisplay:
+    """
+    Gets a specific tag from the database.
+    """
     try:
         tag = await get_tag(tag_id)
     except Exception as e:
@@ -321,8 +512,11 @@ async def tags_get_tag(tag_id: int) -> TagDisplay:
     return TagDisplay(**tag.dict())
 
 
-@app.post("/v1/tags", dependencies=[Depends(api_auth)])
+@app.post("/v1/tags", dependencies=[Depends(api_auth)], tags=["Tags"])
 async def tags_post(tag: Tag) -> TagDisplay:
+    """
+    Creates a new tag.
+    """
     try:
         tag = await create_tag(tag.name, tag.color, tag.textColor)
     except Exception as e:
@@ -332,8 +526,13 @@ async def tags_post(tag: Tag) -> TagDisplay:
     return TagDisplay(**tag.dict())
 
 
-@app.patch("/v1/tags/{tag_id}", dependencies=[Depends(api_auth)])
+@app.patch("/v1/tags/{tag_id}", dependencies=[Depends(api_auth)], tags=["Tags"])
 async def tags_patch(tag_id: int, tag: Tag) -> TagDisplay:
+    """
+    Updates a tag.
+
+    This is a patch meaning you don't have to pass all the data in the tag object.
+    """
     try:
         updated_tag = tag.dict(exclude_unset=True)
         tag = await get_tag(tag_id)
@@ -349,15 +548,20 @@ async def tags_patch(tag_id: int, tag: Tag) -> TagDisplay:
     return TagDisplay(**tag.dict())
 
 
-@app.delete("/v1/tags/{tag_id}", dependencies=[Depends(api_auth)])
-async def tags_delete(tag_id: int) -> dict:
+@app.delete("/v1/tags/{tag_id}", dependencies=[Depends(api_auth)], tags=["Tags"])
+async def tags_delete(tag_id: int) -> bool:
+    """
+    Deletes a tag.
+
+    **CAN NOT BE UNDONE!**
+    """
     try:
         await delete_tag(tag_id)
     except Exception as e:
         _log.error(e)
         raise HTTPException(status_code=500, detail="Internal Server Error")
 
-    return {"message": "Tag Deleted"}
+    return True
 
 
 server = uvicorn.Server(
@@ -384,7 +588,9 @@ class API(Cog):
         name="verify", description="Commands regarding verification"
     )
 
-    @verifyGroup.command(name="link")
+    @verifyGroup.command(
+        name="link", description="Link your Discord account to your Roblox account"
+    )
     async def verify_link(self, interaction: Interaction, key: str):
         await interaction.response.defer(ephemeral=True, thinking=True)
 
@@ -406,7 +612,10 @@ class API(Cog):
         else:
             await interaction.followup.send("Invalid key.", ephemeral=True)
 
-    @verifyGroup.command(name="unlink")
+    @verifyGroup.command(
+        name="unlink",
+        description="Unlink your Discord account from your Roblox account",
+    )
     async def verify_unlink(self, interaction: Interaction):
         await interaction.response.defer(ephemeral=True, thinking=True)
 
@@ -424,8 +633,8 @@ class API(Cog):
 
     @Cog.listener()
     async def on_ready(self):
-        global cog
-        cog = self
+        # global cog
+        # cog = self
         self.bot.loop.create_task(server.serve())
         self.overwrite_uvicorn_logger()
         _log.info(f"Cog {__name__} ready")
